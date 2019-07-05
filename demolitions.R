bff7f6fb4a1c7b036e26727fcf733f3ed03d7fb5
ubelhj <ubelhj@uw.edu>
2019-07-05 13:32:56 -0700

    website runs

--- demolitions.R
+++ demolitions.R
@@ -2,5 +2,16 @@
 
 library(httr)
 library(rdrop2)
+library(dplyr)
+library(data.table)
+library(jsonlite)
+library(shiny)
+library(purrr)
 
-drop_download()
+drop_auth()
+drop_download('/Apps/Demo Leaderboard/leaderboard.csv', overwrite = TRUE)
+
+leaderboard <- read.csv("./leaderboard.csv") 
+  
+leaderboard <- leaderboard[order(-leaderboard$Demos),] %>% 
+  unique()
